eb71d906639c0388cfa911ee7de89554ba895756
Timur Çelikel <me@timurcelikel.com>
2016-05-03 19:44:26 +0200

    add logo to main page

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,25 @@ import { Link } from 'react-router'
 import { prefixLink } from 'gatsby-helpers'
 import DocumentTitle from 'react-document-title'
 import { config } from 'config'
+import Isvg from 'react-inlinesvg'
 
 export default class Sass extends React.Component {
   render () {
     return (
       <DocumentTitle title={config.siteTitle}>
+        
         <div>
-          <h1>
-            Hi people
-          </h1>
-          <Link to={prefixLink('/planetarium/')}>planetarium</Link>
+          <div className="vo-intro">
+            <Link to={prefixLink('/')}>
+              <div className="vo-logo">
+                <Isvg src="vsternchen.svg">
+                </Isvg>
+              </div>
+            </Link>
+          </div>
+          <Link to={prefixLink('/projekte/planetarium/')}>planetarium</Link>
         </div>
+
       </DocumentTitle>
     )
   }
