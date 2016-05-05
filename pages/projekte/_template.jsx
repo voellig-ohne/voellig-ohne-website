212a1e1f63e0aec6f2ef1495f1b4e037f66b6825
Timur Çelikel <me@timurcelikel.com>
2016-05-05 19:58:48 +0200

    navigate in projects animated

--- pages/projekte/_template.jsx
+++ pages/projekte/_template.jsx
@@ -5,19 +5,66 @@ import sternchen from 'pages/sternchen.svg'
 import Header from 'pages/components/_header.js'
 
 module.exports = React.createClass({
-  propTypes () {
-    return {
-      children: React.PropTypes.any,
-    }
-  },
-  render () {
-    return (
-      <div>
-        <Header></Header>
-        <div>
-          {this.props.children}
-        </div>
-      </div>
-    )
-  },
+    propTypes () {
+        return {
+            children: React.PropTypes.any,
+        }
+    },
+    render () {
+        const projectList = [];
+
+        const currentPath = '/projekte/'
+
+        const projects = this.props.route.pages.filter((page) => {
+            return page.path.startsWith(currentPath) && page.path !== currentPath
+        })
+
+        console.log(this.props.location.pathname)
+
+        projects.forEach((page) => {
+            const backgroundImage = {
+                backgroundImage: `url(${page.path}${page.data.mainImage})`
+            }
+
+            const subDir = page.path.replace(currentPath, '')
+
+            let responsiveImage = require('responsive?sizes[]=500,sizes[]=1000,sizes[]=2000!./' + subDir + page.data.mainImage + '.jpg')
+
+            console.log(page.path)
+            let classNames = 'vo_project_list-item';
+
+            if(this.props.location.pathname !== currentPath) {
+                classNames += (this.props.location.pathname === page.path) ? ' active' : ' passive';
+            }
+
+            projectList.push(
+                <li className={classNames}
+                    key={page.path}>
+                    <Link to={prefixLink(page.path)}>
+                        <img srcSet={responsiveImage.srcSet} src={responsiveImage.src} />
+                        <div className="vo_project_list-description">
+                            <h2>{page.data.title}</h2>
+                            <div>{page.data.description}</div>
+                            <div className="vo-trenner"/>
+                            <div>{page.data.date}</div>
+                            <div>{page.data.what}</div>
+                            <div className="vo-trenner"/>
+                            <div>{page.data.what2}</div>
+                        </div>
+                    </Link>
+                </li>
+            )
+        })
+
+        return (
+            <div>
+                <main>
+                    <ul className="vo_project_list">
+                        {projectList}
+                    </ul>
+                </main>
+                {this.props.children}
+            </div>
+        )
+    },
 })
--- pages/projekte/index.js
+++ pages/projekte/index.js
@@ -10,50 +10,10 @@ import './project-list.less'
 export default class Projects extends React.Component {
 
 	render () {
-		const projectList = [];
 
-		const currentPath = this.props.route.page.path
-
-		const projects = this.props.route.pages.filter((page) => {
-			return page.path.startsWith(currentPath) && page.path !== currentPath
-		})
-
-		projects.forEach((page) => {
-			const backgroundImage = {
-				backgroundImage: `url(${page.path}${page.data.mainImage})`
-			}
-
-			const subDir = page.path.replace(currentPath, '')
-
-			let responsiveImage = require('responsive?sizes[]=500,sizes[]=1000,sizes[]=2000!./' + subDir + page.data.mainImage + '.jpg')
-
-			projectList.push(
-				<li className="vo_project_list-item"
-					key={page.path}>
-					<Link to={prefixLink(page.path)}>
-						<img srcSet={responsiveImage.srcSet} src={responsiveImage.src} />
-						<div className="vo_project_list-description">
-							<h2>{page.data.title}</h2>
-							<div>{page.data.description}</div>
-							<div className="vo-trenner"/>
-							<div>{page.data.date}</div>
-							<div>{page.data.what}</div>
-							<div className="vo-trenner"/>
-							<div>{page.data.what2}</div>
-						</div>
-					</Link>
-				</li>
-			)
-		})
 
     return (
 		<DocumentTitle title={`${config.siteTitle} | projekte`}>
-
-			<main>
-				<ul className="vo_project_list">
-					{projectList}
-				</ul>
-			</main>
 
 		</DocumentTitle>
     )
