52f08acd9bde70cd7f3f23d0d7611cc4d46eb135
Timur Çelikel <me@timurcelikel.com>
2016-05-09 20:04:06 +0200

    index.html: proper indentation

--- html.js
+++ html.js
@@ -3,38 +3,38 @@ import DocumentTitle from 'react-document-title'
 import { prefixLink } from 'gatsby-helpers'
 
 module.exports = React.createClass({
-  propTypes () {
-    return {
-      title: React.PropTypes.string,
-    }
-  },
-  render () {
-    const title = DocumentTitle.rewind()
+    propTypes () {
+        return {
+            title: React.PropTypes.string,
+        }
+    },
+    render () {
+        const title = DocumentTitle.rewind()
 
-    let cssLink
-    if (process.env.NODE_ENV === 'production') {
-      cssLink = <link rel="stylesheet" href={prefixLink('/styles.css')} />
-    }
+        let cssLink
+        if (process.env.NODE_ENV === 'production') {
+            cssLink = <link rel="stylesheet" href={prefixLink('/styles.css')} />
+        }
 
-    return (
-      <html lang="en">
-        <head>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0 maximum-scale=1.0, user-scalable=no"
-          />
-          <meta name="description" content="völlig ohne. entwickelt, gestaltet, setzt um.">
-          <title>{title}</title>
-          <link rel="shortcut icon" href={this.props.favicon} />
-          {cssLink}
-        </head>
-        <body>
-          <div id="react-mount" dangerouslySetInnerHTML={{ __html: this.props.body }} />
-          <script src={prefixLink('/bundle.js')} />
-        </body>
-      </html>
-    )
-  },
-})
+        return (
+            <html lang="en">
+                <head>
+                    <meta charSet="utf-8" />
+                    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1.0 maximum-scale=1.0, user-scalable=no"
+                        />
+                    <meta name="description" content="völlig ohne. entwickelt, gestaltet, setzt um.">
+                        <title>{title}</title>
+                        <link rel="shortcut icon" href={this.props.favicon} />
+                        {cssLink}
+                    </head>
+                    <body>
+                        <div id="react-mount" dangerouslySetInnerHTML={{ __html: this.props.body }} />
+                        <script src={prefixLink('/bundle.js')} />
+                    </body>
+                </html>
+            )
+        },
+    })
