816bb246a2dc4deb8e2945ff7c7bfac9e95448e7
Timur Çelikel <me@timurcelikel.com>
2016-05-24 18:40:42 +0200

    revert to not inlined css because it does not work anymore

--- html.js
+++ html.js
@@ -15,7 +15,8 @@ module.exports = React.createClass({
 
         let css
         if (process.env.NODE_ENV === 'production') {
-            css = <style dangerouslySetInnerHTML={{ __html: require('!raw!./public/styles.css') }} />
+            css = <link rel="stylesheet" href={prefixLink('/styles.css')} />
+        //    css = <style dangerouslySetInnerHTML={{ __html: require('!raw!./public/styles.css') }} />
         }
 
         return (
