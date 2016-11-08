fa8e68d631a4cea419dfbe9e904538daf8201ba2
Timur Çelikel <me@timurcelikel.com>
2016-11-08 17:24:55 +0100

    remove app-module-path

--- scripts/generate_rss.js
+++ scripts/generate_rss.js
@@ -1,9 +1,7 @@
 // taken from / inspired by https://github.com/scottnonnenberg/blog thanks!
 
-import './util/setupModulePath';
-
-import loadPosts from 'scripts/util/loadPosts';
-import buildFeeds from 'scripts/util/buildFeeds';
+import loadPosts from './util/loadPosts';
+import buildFeeds from './util/buildFeeds';
 
 const posts = loadPosts({
   limit: 20,
--- scripts/util/setupModulePath.js
+++
@@ -1,4 +0,0 @@
-import path from 'path';
-import modulePath from 'app-module-path';
-
-modulePath.addPath(path.join(__dirname, '../..'));
