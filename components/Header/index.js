26bdc80d12b4f34f0ebb9fdbebe0b0501acc5439
Timur Çelikel <me@timurcelikel.com>
2017-06-20 17:27:29 +0200

    Header: link to /projekte when on landing page

--- components/Header/index.js
+++ components/Header/index.js
@@ -7,12 +7,15 @@ import style from './style.module.less'
 
 export default class Header extends React.Component {
     render () {
+        const { isRoot } = this.props
         const v = require('!svg-inline-loader!./v.svg')
         const sternchen = require('!svg-inline-loader!./sternchen.svg')
 
+        const link = isRoot ? '/projekte/' : '/'
+
         return (
             <nav className={style.header}>
-                <Link to={prefixLink('/')} className={style.logo}>
+                <Link to={link} className={style.logo}>
                     <span className={style.v} dangerouslySetInnerHTML={{ __html: v }} />
                     <span className={style.sternchen} dangerouslySetInnerHTML={{ __html: sternchen }} />
                 </Link>
