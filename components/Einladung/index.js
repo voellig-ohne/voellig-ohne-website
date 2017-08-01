a77b15bf9630e35a16b0f8c8ae37f2cd82184ccd
Timur Çelikel <me@timurcelikel.com>
2017-08-01 16:36:17 +0200

    Einladung: first working version

--- components/Einladung/index.js
+++ components/Einladung/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOMServer from 'react-dom/server';
 import { Link } from 'react-router';
 import { prefixLink } from 'gatsby-helpers';
 import { map } from 'lodash';
@@ -8,6 +9,8 @@ import style from './style.module.less';
 
 import ANTWORT from './antwort.json';
 
+const MAIL_SERVER = '/sendmail.php/';
+
 export default class Einladung extends React.Component {
     constructor(props) {
         super(props);
@@ -15,8 +18,19 @@ export default class Einladung extends React.Component {
             antwort: map(ANTWORT, row => {
                 return map(row, word => false);
             }),
+            sendStatus: 'UNSENT'
         };
         this.toggleWord = this.toggleWord.bind(this);
+        this.send = this.send.bind(this);
+
+        if (typeof window !== 'undefined') {
+            this.guest = QueryString();
+        } else {
+            this.guest = {
+                name: 'NONAME',
+                mail: 'NOMAIL',
+            };
+        }
     }
 
     render() {
@@ -24,15 +38,17 @@ export default class Einladung extends React.Component {
             <div className="vo-section_wrapper">
                 <section className="vo-section">
                     <h2>
-                        einladung <del>ausladung hinweis</del>
+                        einladung <del>ausladung hinweis</del> für <del>von</del> {this.guest.name}
                     </h2>
                     <p>
-                        <del>eröffnung jubiläum umzug</del> werkschau <del>neueröffnung räumungsverkauf schließung</del>
+                        <del>eröffnung jubiläum umzug</del> werkschau <del>neueröffnung räumungsverkauf schließung</del>{' '}
                         <br />
                         <del>mo di mi do</del> fr <del>sa so</del>
                         <br />
-                        <del>1 2 3 4 5 6 7</del> 8{' '}
-                        <del>9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31</del>
+                        <span className={style.dates}>
+                            <del>1 2 3 4 5 6 7</del> 8{' '}
+                            <del>9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31</del>
+                        </span>
                         <br />
                         <del>jan feb mär apr jun jul aug</del> sep <del>okt nov dez</del>
                         <br />
@@ -50,7 +66,9 @@ export default class Einladung extends React.Component {
                 <section className="vo-section">
                     <h2>Antwort (nicht zutreffendes streichen)</h2>
                     <Antwort antwort={ANTWORT} antwortState={this.state.antwort} onClick={this.toggleWord} />
-                    <button className={style.button}>Antwort abschicken</button>
+                    <button className={style.button} onClick={this.send}>
+                        Antwort abschicken
+                    </button>
                 </section>
             </div>
         );
@@ -61,6 +79,39 @@ export default class Einladung extends React.Component {
         this.setState({
             antwort: this.state.antwort,
         });
+    }
+
+    send() {
+        const antwortString = ReactDOMServer.renderToStaticMarkup(
+            AntwortToSend({ antwort: ANTWORT, antwortState: this.state.antwort })
+        );
+
+        const XHR = sendData({
+            email: this.guest.mail,
+            name: this.guest.name,
+            comment: antwortString,
+        });
+
+        this.setState({
+            sendStatus: 'SENDING',
+        });
+
+        XHR.onreadystatechange = event => {
+            if (XHR.readyState === 4) {
+                if (XHR.status === 200) {
+                    console.log('Yeah! Data sent and response loaded.', XHR);
+
+                    this.setState({
+                        sendStatus: 'SENT',
+                        response: XHR.responseText,
+                    });
+                } else {
+                    this.setState({
+                        sendStatus: 'SENT',
+                    });
+                }
+            }
+        };
     }
 }
 
@@ -90,3 +141,82 @@ function Antwort({ antwort, antwortState, onClick }) {
         </p>
     );
 }
+
+function AntwortToSend({ antwort, antwortState }) {
+    return (
+        <p>
+            {map(antwort, (row, rowidx) => {
+                return (
+                    <span key={rowidx}>
+                        {map(row, (word, wordidx) => {
+                            return (
+                                <span key={wordidx}>
+                                    {antwortState[rowidx][wordidx]
+                                        ? <del>
+                                              {word}{' '}
+                                          </del>
+                                        : <span>
+                                              {word}{' '}
+                                          </span>}
+                                </span>
+                            );
+                        })}
+                        <br />
+                    </span>
+                );
+            })}
+        </p>
+    );
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Forms/Sending_forms_through_JavaScript
+
+function sendData(data) {
+    var XHR = new XMLHttpRequest();
+    var urlEncodedData = '';
+    var urlEncodedDataPairs = [];
+    var name;
+
+    // We turn the data object into an array of URL encoded key value pairs.
+    for (name in data) {
+        urlEncodedDataPairs.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]));
+    }
+
+    // We combine the pairs into a single string and replace all encoded spaces to
+    // the plus character to match the behaviour of the web browser form submit.
+    urlEncodedData = urlEncodedDataPairs.join('&').replace(/%20/g, '+');
+
+    // We setup our request
+    XHR.open('POST', MAIL_SERVER);
+
+    // We add the required HTTP header to handle a form data POST request
+    XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    // And finally, We send our data.
+    XHR.send(urlEncodedData);
+
+    return XHR;
+}
+
+// http://stackoverflow.com/questions/979975/how-to-get-the-value-from-the-url-parameter
+
+var QueryString = () => {
+    var query_string = {};
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        // If first entry with this name
+        if (typeof query_string[pair[0]] === 'undefined') {
+            query_string[pair[0]] = decodeURIComponent(pair[1]);
+            // If second entry with this name
+        } else if (typeof query_string[pair[0]] === 'string') {
+            var arr = [query_string[pair[0]], decodeURIComponent(pair[1])];
+            query_string[pair[0]] = arr;
+            // If third or later entry with this name
+        } else {
+            query_string[pair[0]].push(decodeURIComponent(pair[1]));
+        }
+    }
+    return query_string;
+};
