19e6143f00729749e455dd6b5f00473583128696
Timur Çelikel <me@timurcelikel.com>
2017-08-08 14:42:20 +0200

    MailWrapper: add nice formatting to the mails

---
+++ components/MailWrapper/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import sternchen from './sternchen.png';
+
+function MailWrapper({ children }) {
+    return (
+        <div
+            style={{
+                background: '#f8f9f6',
+                fontFamily: 'courier new,courier',
+            }}
+        >
+            <div style={{ textAlign: 'center', paddingTop: '1rem' }}>
+                <img src={sternchen} style={{ width: '46px' }} />
+            </div>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                }}
+            >
+                <div style={{ background: 'white', margin: '1rem', padding: '1rem', maxWidth: '700px' }}>
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MailWrapper;
