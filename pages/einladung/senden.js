c516c1e518b7fd28714abff7b8c8df85c42b0595
Timur Çelikel <me@timurcelikel.com>
2017-08-08 14:42:57 +0200

    Senden: small internal page to send out invites

---
+++ pages/einladung/senden.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'react-router';
+import EinladungSenden from 'components/EinladungSenden';
+
+export default class Index extends React.Component {
+    render() {
+        return <EinladungSenden />;
+    }
+}
